270ca471fa36f04cf32e3d6f2a471ac6f176326b
Sang Rok Lee <lsrock1@naver.com>
2017-01-13 05:08:37 +0000

    add button view helper in index

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,4 +2,16 @@ module HomeHelper
   def menu_cache_key menu
     "menus-#{menu['name']}-#{menu['update']}-#{menu['id']}"
   end
+  
+  def lan_button(language,day)
+    capture do
+      language.collect{|key,value|
+      concat(
+        content_tag(:li) do
+          content_tag(:a,value[0],class: "btn-floating align-center "+value[1],href: '/'+key.to_s+'/'+day.to_s)
+        end
+        )
+      }
+    end
+  end
 end
