db68361d1f1d99e33665dd5f7ab0374f84990e04
Sang Rok Lee <lsrock1@naver.com>
2017-01-28 12:31:34 +0000

    hybrid add

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,6 +1,6 @@
 module HomeHelper
   def menu_cache_key menu
-    "menus-#{menu['name']}-#{menu['update']}-#{menu['id']}"
+    "menus-#{menu['name']}-#{menu['update']}-#{menu['id']}-bride"
   end
   
   def tabs(day)
@@ -31,16 +31,35 @@ module HomeHelper
   end
   
   def card_top(menu)
-      content_tag(:div,class: 'card-content') do
-        concat content_tag(:i,"restaurant".html_safe,class: "material-icons rotate red-text text-lighten-2",'data-angle' => 0,style: "display:none;")
-        concat content_tag(:span,menu['name'].titleize,class: 'card-title')
-        concat content_tag(:i,'alarm_on'.html_safe,class: 'material-icons grey-text alarm right z-depth-1','data-id' => menu['main'].id, style: "display:none;")
-        concat content_tag(:div,"#{menu['time']} / #{menu['price']}",class: "caf_times")
-        concat content_tag(:div,menu['menu'].shift.titleize,class: "caf_rep")
-        concat(content_tag(:p) do
-          concat card_content(menu['menu'])
-        end)
-      end
+    content_tag(:div,class: 'card-content card_top') do
+      concat content_tag(:span, menu['name'].titleize,class: 'card-title')
+      concat tag('br')
+      concat (content_tag(:div,class: 'grey-text') do
+        concat "#{menu['time']} / #{menu['price']}"
+        concat content_tag(:span,"#{menu['kcal']}",class: :right )
+      end)
+    end
+  end
+  
+  def card_middle(menu)
+    content_tag(:div,class: 'card-content card_middle') do
+      concat content_tag(:span,menu['menu'].shift.titleize,class: "caf_main")
+      concat(content_tag(:p,class: 'caf_menu') do
+        concat card_content(menu['menu'])
+      end)
+      concat (content_tag(:div,class: 'caf_ingre') do
+        if menu['ingre']!=nil
+          concat menu['ingre'].map{|item| item.titleize}.join(",")
+        end
+      end)
+    end
+  end
+  
+  def card_bottom(menu)
+    content_tag(:div,class: 'card-action card-icon') do
+      concat content_tag(:div,content_tag(:i,'alarm_off'.html_safe,class: 'material-icons'),class:  'grey-text alarm','data-id' => menu['main'].id,'data-name' => menu['main'].kname, style: "display:none;")
+      concat like_icon(menu)    
+    end
   end
   
   def card_content(menu)
@@ -66,56 +85,30 @@ module HomeHelper
   
   def card_img(menu)
     if menu['main'].u_picture!=nil&&menu['main'].u_picture!=""
-      content_tag(:div,class: 'card-image') do
+      content_tag(:div,class: 'card-image c-image') do
         concat image_tag(menu['main'].u_picture,class: 'u_picture')
       end
     end
   end
   
-  def card_bottom_left(menu)
-    content_tag(:div, class: "card-bottom-left") do
-      concat(content_tag(:p) do
-        if menu['ingre']!=nil
-          concat menu['ingre'].map{|item| item.titleize}.join(",")
-        end
-        concat "&nbsp;".html_safe
-      end)
-    end
-  end
-  
-  def card_bottom_middle(menu)
-    content_tag(:div, class: "card-bottom-middle") do
-      concat(content_tag(:p) do
-        concat "#{menu['kcal']}"
-        concat "&nbsp;".html_safe
-      end)
-    end
-  end
-  
-  def card_bottom_right(menu)
+  def like_icon(menu)
     if cookies[menu['main'].kname.to_sym]=="1"||session[menu['main'].kname.to_sym]=="1"
-      content_tag(:div, class: "card-bottom-right like white red-text text-lighten-3",id: menu['main'].id) do
-        concat (content_tag(:p) do
-          concat content_tag(:i,'favorite',class: "material-icons")
-          concat "&nbsp;&nbsp;".html_safe
-          concat menu['main'].u_like
-        end)
+      content_tag(:div,class: 'like red-text text-lighten-3','data-id' => menu['main'].id,'data-name' => menu['main'].kname) do
+        concat content_tag(:i,'favorite',class: "material-icons")
+        concat "&nbsp;&nbsp;".html_safe
+        concat menu['main'].u_like
       end
     elsif cookies[menu['main'].kname.to_sym]=="0"||session[menu['main'].kname.to_sym]=="0"
-      content_tag(:div, class: "card-bottom-right like white grey-text",id: menu['main'].id) do
-        concat (content_tag(:p) do
-          concat content_tag(:i,'favorite_border',class: "material-icons")
-          concat "&nbsp;&nbsp;".html_safe
-          concat menu['main'].u_like
-        end)
+      content_tag(:div,class: 'like grey-text','data-id' => menu['main'].id,'data-name' => menu['main'].kname) do
+        concat content_tag(:i,'favorite_border',class: "material-icons")
+        concat "&nbsp;&nbsp;".html_safe
+        concat menu['main'].u_like
       end
     elsif cookies[menu['main'].kname.to_sym]==nil||session[menu['main'].kname.to_sym]==nil
-      content_tag(:div, class: "card-bottom-right like white grey-text",id: menu['main'].id) do
-        concat (content_tag(:p) do
-          concat content_tag(:i,'favorite_border',class: "material-icons")
-          concat "&nbsp;&nbsp;".html_safe
-          concat menu['main'].u_like
-        end)
+      content_tag(:div,class: 'like grey-text', 'data-id' => menu['main'].id,'data-name' => menu['main'].kname) do
+        concat content_tag(:i,'favorite_border', class: "material-icons")
+        concat "&nbsp;&nbsp;".html_safe
+        concat menu['main'].u_like
       end
     end
   end
