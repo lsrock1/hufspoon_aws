f9474dda8ea09e1aeb95580c7542b65753ec3ebf
sang rok lee <lsrock1@naver.com>
2017-03-10 11:03:37 +0100

    category chagned

--- app/controllers/concerns/Getlist.rb
+++ app/controllers/concerns/Getlist.rb
@@ -17,12 +17,7 @@ module Getlist
   def restCategoryHash
     return{#쿼리 => [영어,독음,중국어,일어,한국어]
       "한식" => ["Korean", "Korean","Korean","Korean","한식"],
-      "일식" => ["Japanese", "Japanese","Japanese","Japanese","일식"],
-      "양식" => ["Western", "Western","Western","Western","양식"],
-      "중식" => ["Chinese", "Chinese","Chinese","Chinese","중식"],
-      "치킨" => ["Chicken", "Chicken","Chicken","Chicken","치킨"],
-      "고기" => ["Meat", "Meat","Meat","Meat","고기"],
-      "분식/면" => ["Snack", "Snack","Snack","Snack","분식/면"]
+      "세계" => ["World", "World","World","World","세계"]
     }
   end
   
