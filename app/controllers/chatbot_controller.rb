397f7ba5b788b555b24491f58c94aa7bde47b915
Sang Rok Lee <lsrock1@naver.com>
2017-09-20 05:13:31 +0000

    bug fix

--- app/controllers/chatbot_controller.rb
+++ app/controllers/chatbot_controller.rb
@@ -221,7 +221,7 @@ class ChatbotController < ApplicationController
         
         @day = time.year.to_s + mm.to_s + dd.to_s
         model = humanities.delete(content)
-        buttons = humanities.values.make_list(@day, @id).select{|item| !item[:main].u_picture.empty?}.map{|item| "Image: #{item[:name].titleize}"}
+        buttons = humanities.values.map{|item| item.make_list(@day, @id)}.select{|item| !item[:main].u_picture.empty?}.map{|item| "Image: #{item[:name].titleize}"}
         list = model.make_list(@day, @id)
         
         unless list[:main].u_picture.empty?
